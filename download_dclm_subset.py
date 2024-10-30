c77397e317ca4b5ee0310d0b842e35d8222caaf0
LeoXinhaoLee <Mc1114207918@outlook.com>
2024-10-29 21:59:43 -0700

    Update download link format to avoid download html

--- download_dclm_subset.py
+++ download_dclm_subset.py
@@ -15,7 +15,7 @@ file_abs_id = [i for i in range(num_total_file)]
 
 sampled_abs_id = random.sample(file_abs_id, math.ceil(num_total_file / 40))  # 4T tokens -> 100B tokens
 
-link_format = 'https://huggingface.co/datasets/mlfoundations/dclm-baseline-1.0/blob/main/global-shard_{:02}_of_10/local-shard_{}_of_10/shard_{:08}_processed.jsonl.zst'
+link_format = 'https://huggingface.co/datasets/mlfoundations/dclm-baseline-1.0/blob/main/global-shard_{:02}_of_10/local-shard_{}_of_10/shard_{:08}_processed.jsonl.zst?download=true'
 
 link_list = []
 for abs_id in sampled_abs_id:
@@ -25,9 +25,25 @@ for abs_id in sampled_abs_id:
     link = link_format.format(global_id + 1, local_id, file_id)
     link_list.append(link)
 
-# with open("links.txt", "w") as f:
-#     for link in link_list:
-#         f.write(link + "\n")
+with open("links.txt", "w") as f:
+    for link in link_list:
+        f.write(link + "\n")
 
 print(len(link_list))
 print(len(set(link_list)))
+
+# import zstandard as zstd
+# import json
+# with open("/Users/xinhaoli/Desktop/shard_00000000_processed.jsonl.zst", "rb") as compressed_file:
+#     dctx = zstd.ZstdDecompressor()
+#     with dctx.stream_reader(compressed_file) as reader:
+#         # Read entire decompressed content at once
+#         decompressed_data = reader.read().decode('utf-8')
+#
+#         # Split the content into lines
+#         lines = decompressed_data.splitlines()
+#
+#         # Parse each line as JSON
+#         for line in lines:
+#             line_content = json.loads(line)
+#             print(line_content)
